cb2171dd9a96a6078048bd1c87409eb80895e007
Mike Gray <mike@mikegray.org>
2025-09-18 18:35:26 -0400

    fix: general cleanup

--- examples/message-formatter.js
+++ examples/message-formatter.js
@@ -1,13 +1,15 @@
-var winston = require('winston'),
-    WinstonCloudWatch = require('../index');
+const winston = require("winston");
+const WinstonCloudWatch = require("../index");
 
-winston.add(new WinstonCloudWatch({
-  messageFormatter: function() {
-    return 'will always log this text'
-  },
-  logGroupName: 'testing',
-  logStreamName: 'first',
-  awsRegion: 'us-east-1'
-}));
+winston.add(
+  new WinstonCloudWatch({
+    messageFormatter: function () {
+      return "will always log this text";
+    },
+    logGroupName: "testing",
+    logStreamName: "first",
+    awsRegion: "us-east-1",
+  })
+);
 
-winston.error('this will be always overwritten');
+winston.error("this will be always overwritten");
--- examples/simulate/running-process.js
+++ examples/simulate/running-process.js
@@ -1,29 +1,28 @@
 console.log(process.pid);
 
-var winston = require('winston'),
-    WinstonCloudWatch = require('../../index');
+const winston = require("winston");
+const WinstonCloudWatch = require("../../index");
 
 winston.transports.CloudWatch1 = WinstonCloudWatch;
 winston.transports.CloudWatch2 = WinstonCloudWatch;
 
-winston.loggers.add('category1', {
+winston.loggers.add("category1", {
   CloudWatch1: {
-    logGroupName: 'test',
-    logStreamName: 'test'
-  }
+    logGroupName: "test",
+    logStreamName: "test",
+  },
 });
-winston.loggers.add('category2', {
+winston.loggers.add("category2", {
   CloudWatch2: {
-    logGroupName: 'test',
-    logStreamName: 'test'
-  }
+    logGroupName: "test",
+    logStreamName: "test",
+  },
 });
 
-winston.loggers.get('category1').error(new Date());
-winston.loggers.get('category2').error(new Date());
+winston.loggers.get("category1").error(new Date());
+winston.loggers.get("category2").error(new Date());
 
-process.on('SIGUSR1', function() {
-  winston.loggers.get('category1').error(new Date());
-  winston.loggers.get('category2').error(new Date());
+process.on("SIGUSR1", function () {
+  winston.loggers.get("category1").error(new Date());
+  winston.loggers.get("category2").error(new Date());
 });
-
